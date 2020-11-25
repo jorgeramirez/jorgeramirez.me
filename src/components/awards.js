01af390ea6ed95c3b5cfb2a4890c8c01ca3ce7e3
Jorge Ramírez <jorgeramirez1990@gmail.com>
2020-11-24 22:25:25 -0300

    update
    Signed-off-by: Jorge Ramírez <jorgeramirez1990@gmail.com>

--- src/components/awards.js
+++ src/components/awards.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Awards = () => (
   <div>
-    <h4> Awards </h4>
+    <h4> <span role="img" aria-label="award">🏆</span> Awards </h4>
     <ul>
       <li>
         <a href="https://cscw.acm.org/2020/awards/" target="_blank" rel="noopener noreferrer">
-          Outstanding poster
+        Outstanding poster
         </a>{' '}
         recognition at CSCW 2020 for our work{' '}
         <a href="https://dl.acm.org/doi/abs/10.1145/3406865.3418318" target="_blank" rel="noopener noreferrer">
