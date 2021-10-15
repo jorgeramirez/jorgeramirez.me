2a5872ba6742773e0ad7189701befbcae82f5a3b
Jorge Ramírez <jorgeramirez1990@gmail.com>
2021-10-15 18:44:19 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1456e818618286f90d3a.js"
+    "url": "webpack-runtime-05767834d6b615eeca99.js"
   },
   {
     "url": "styles.1ccb6da314e392a77459.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7d6e1016c5038cddf6446dc34a6f781d"
+    "revision": "469233cbf539a1605f61289df8bba6ec"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-d3ee03be2ef85643f368.js"
