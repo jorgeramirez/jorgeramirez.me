44a7fea87a616ab2f3dfb58d8b7396752770c197
Jorge Ramírez <jorgeramirez1990@gmail.com>
2021-12-03 23:38:57 +0100

    update
    Signed-off-by: Jorge Ramírez <jorgeramirez1990@gmail.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,14 +58,6 @@ exports.createPages = async ({ graphql, actions }) => {
 }
 
 
-const express= require('express');
-
-exports.onCreateDevServer=({app})=>{
-    app.use(express.static('public'))
-}
-
-
-
 exports.onCreatePage = async ({ page, actions: { deletePage } }) => {
   if (page.path.match(/^\/blog/)) {
     deletePage(page)
